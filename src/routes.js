ac1afb83354314ac863f87238aad39d7c3a30f60
musairdev <musairdev@gmail.com>
2024-03-20 12:14:10 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+const routes = [
+    {
+      method: 'POST',
+      path: '/notes',
+      handler: addNoteHandler,
+    },
+    {
+      method: 'GET',
+      path: '/notes',
+      handler: () => {},
+    },
+  ];
